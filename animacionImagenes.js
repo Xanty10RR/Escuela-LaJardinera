32cc36889d5e87a1c45ef0f40beb30595e162b24
Santiago <santiagocajamarca.@gmail.com>
2025-08-27 19:15:35 -0500

    cambios de interfaz

--- animacionImagenes.js
+++ animacionImagenes.js
@@ -10,3 +10,21 @@ function showNextImage() {
 }
 
 setInterval(showNextImage, 4000);
+  const prevBtn = document.getElementById("prev");
+  const nextBtn = document.getElementById("next");
+
+  let index = 0;
+
+  function showSlide(i) {
+    if (i < 0) index = images.length - 1;
+    else if (i >= images.length) index = 0;
+    else index = i;
+
+    slides.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  prevBtn.addEventListener("click", () => showSlide(index - 1));
+  nextBtn.addEventListener("click", () => showSlide(index + 1));
+
+  // Autoplay cada 5s
+  setInterval(() => showSlide(index + 1), 4000);
